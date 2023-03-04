06bce6c647224ef87ceae1fbf8fc249f32b74f1b
JosiasPires <supergc59@gmail.com>
2023-03-04 20:29:10 -0300

    Create files

---
+++ app.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(opr, a, b) {
+    switch (opr) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return 'Error'
+    }
+}
+
