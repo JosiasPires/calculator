cca0e6145517fa2c4c1800506ae9c99d7b261566
JosiasPires <supergc59@gmail.com>
2023-03-05 18:29:56 -0300

    Fix calculator

--- app.js
+++ app.js
@@ -35,9 +35,68 @@ const commands = Array.from(document.querySelectorAll('.opr'));
 const clear = document.querySelector('.ac');
 const del = document.querySelector('.del');
 const calc = document.querySelector('.calc');
-let Operator;
+let operator;
 let numA;
 let numB;
 let value = '';
 let result;
 
+function resetCalc() {
+    operator = undefined;
+    numA = undefined;
+    numB = undefined;
+    value = '';
+    result = undefined;
+    display.textContent = '0';
+}
+
+function addNumber() {
+    value += this.textContent;
+    display.textContent = value;
+}
+
+function setNumAndOpr() {
+    operator = this.textContent;
+    if (numA === undefined && numB === undefined && value !== '') {
+        numA = value;
+        console.log('Armazenou no A');
+        value = '';
+        display.textContent = '';
+    }
+    else if (numA !== undefined && numB === undefined) {
+        calculate();
+        numA = value;
+        value = '';
+        numB = undefined;
+        // operator = undefined;
+    }
+}
+
+function calculate() {
+    if (numA !== undefined && numB === undefined) {
+        numB = value;
+        console.log('Armazenou no B e calculou')
+        result = operate(operator, +numA, +numB);
+        value = String(result);
+        display.textContent = value;
+        numA = undefined;
+    }
+    else if (numA !== undefined && numB !== undefined) {
+        console.log('passou aqui')
+        numA = numB;
+        numB = undefined;
+    }
+    numB = undefined;
+}
+
+clear.addEventListener('click', resetCalc);
+
+numbers.forEach(number => {
+    number.addEventListener('click', addNumber)
+})
+
+commands.forEach(command => {
+    command.addEventListener('click', setNumAndOpr)
+})
+
+calc.addEventListener('click', calculate);
