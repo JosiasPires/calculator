9d6e1993c7cd25dbd8eb6e31c4bd2a602ffad702
JosiasPires <supergc59@gmail.com>
2023-03-04 22:56:57 -0300

    Make calculator work

--- app.js
+++ app.js
@@ -20,23 +20,68 @@ function operate(opr, a, b) {
             return add(a, b);
         case '-':
             return subtract(a, b);
-        case '*':
+        case 'x':
             return multiply(a, b);
         case '/':
             return divide(a, b);
+        case 'AC':
+            return;
         default:
-            return 'Error'
+            result = 'Error';
     }
 }
 
 const display = document.querySelector('.result');
 const numbers = Array.from(document.querySelectorAll('.num'));
+const commands = Array.from(document.querySelectorAll('.opr'));
+const calc = document.querySelector('.calc');
+let operator;
+let numA;
+let numB;
 let value = '';
+let result;
 
 numbers.forEach(number => {
     number.addEventListener('click', () => {
-        value += number.textContent;
-        display.textContent = value;
-
+        if (!(value.includes('.') && number.textContent === '.')) {
+            value += number.textContent;
+            display.textContent = value;
+        }
+        if ((value === '' || value === '0') && number.textContent === '.') {
+            value = '0.';
+            display.textContent = value;
+        }
     })
+})
+
+commands.forEach(command => {
+    command.addEventListener('click', () => {
+        operator = command.textContent;
+        if (operator === 'AC') {
+            numA = 0;
+            numB = 0;
+            result = 0;
+            display.textContent = 0;
+        }
+        if (typeof numA === 'number') {
+            numB = 0 + Number(value);
+            result = operate(operator, numA, numB);
+            numA = result;
+            display.textContent = result;            
+        }
+        if (typeof result === 'number') {
+            numA = result;
+        }
+        else {
+            numA = Number(value);
+        }
+        value = '';
+    })
+})
+
+calc.addEventListener('click', () => {
+    numB = 0 + Number(value);
+    result = operate(operator, numA, numB);
+    numA = result;
+    display.textContent = result;
 })
