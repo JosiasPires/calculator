c2fc18b9a486b3e94c9dcffeaf00e8282f3fe811
JosiasPires <supergc59@gmail.com>
2023-03-05 17:41:43 -0300

    Clear and start again

--- app.js
+++ app.js
@@ -24,8 +24,6 @@ function operate(opr, a, b) {
             return multiply(a, b);
         case '/':
             return divide(a, b);
-        case 'AC':
-            return;
         default:
             result = 'Error';
     }
@@ -34,84 +32,12 @@ function operate(opr, a, b) {
 const display = document.querySelector('.result');
 const numbers = Array.from(document.querySelectorAll('.num'));
 const commands = Array.from(document.querySelectorAll('.opr'));
+const clear = document.querySelector('.ac');
+const del = document.querySelector('.del');
 const calc = document.querySelector('.calc');
-let operator;
+let Operator;
 let numA;
 let numB;
 let value = '';
 let result;
 
-numbers.forEach(number => {
-    number.addEventListener('click', () => {
-        if (typeof numA === 'number') {
-            value = '';
-        }
-        if (!(value.includes('.') && number.textContent === '.')) {
-            value += number.textContent;
-            display.textContent = value;
-        }
-        if ((value === '' || value === '0') && number.textContent === '.') {
-            value = '0.';
-            display.textContent = value;
-        }
-    })
-})
-
-commands.forEach(command => {
-    command.addEventListener('click', () => {
-        operator = command.textContent;
-        
-        if (operator === 'AC') {
-            numA = undefined;
-            numB = undefined;
-            value = '';
-            result = undefined;
-            display.textContent = 0;
-        }
-        else {
-            if (operator === 'DEL') {
-                console.log(value);
-            }
-            else {
-                if (typeof numA === 'number' && operator !== undefined && value !== '') {
-                    numB = Number(value);
-                    console.log('armazenou no B')
-                    result = operate(operator, numA, numB);
-                    numA = result;
-                    display.textContent = result;
-                    value = '';
-                    numB = undefined;  
-                    console.log('calculando a partir do operador'); 
-                }
-                if (typeof result === 'number') {
-                    numA = result;
-                    console.log('typeof result = number');
-                    console.log('o result é' + result);
-                }
-                else {
-                    if (value !== '') {
-                        numA = Number(value);
-                        console.log('armazenou no A');
-                        value = '';
-                    }
-                }
-            }
-        }
-        // value = '';
-    })
-})
-
-calc.addEventListener('click', () => {
-    if (operator !== undefined) {
-        numB = Number(value);
-        result = operate(operator, numA, numB);
-        numA = value;
-        display.textContent = result;
-        value = String(result);
-        operator = undefined;
-        // numA = undefined;
-        numB = undefined;
-        result = undefined;
-        console.log('jogou o resultado');
-    }
-})
